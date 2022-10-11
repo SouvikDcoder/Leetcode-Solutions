0b33ce2a4ffd922e39909a06678d94cd94dae939
Souvik Dey <souvikpostjob01@gmail.com>
2022-10-11 11:29:32 +0530

    made some files and folder changes

---
+++ python/search-insert-position.py
@@ -0,0 +1,25 @@
+from bisect import bisect_left
+
+def take_closest(myList, myNumber):
+    """
+    Assumes myList is sorted. Returns closest value to myNumber.
+
+    If two numbers are equally close, return the smallest number.
+    """
+    pos = bisect_left(myList, myNumber)
+    if pos == 0:
+        return myList[0]
+    if pos == len(myList):
+        return myList[-1]
+    before = myList[pos - 1]
+    after = myList[pos]
+    if after - myNumber < myNumber - before:
+        return after
+    else:
+        return before
+
+
+nums = [1,3,5,6]
+target = 4
+
+print(take_closest(nums, target))
